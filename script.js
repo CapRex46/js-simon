5e929f624738f9e5e3bed15d625c97baeb881dfe
agres <ale.agresta46@gmail.com>
2021-12-21 19:48:33 +0100

    fix

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 
-function gerRandom(min, max) {
+function getRandom(min, max) {
     return Math.round(Math.random() * (max - min)) + min;
 }
 
 const numbers = [];
 
 
-while (numbers.length < 5) {
-    const randomNumber = gerRandom(1, 10);
+while (numbers.length < 2) {
+    const randomNumber = getRandom(1, 10);
     if (!numbers.includes(randomNumber)){
         numbers.push(randomNumber)
     }
 }
 
-document.write(numbers);
+setTimeout(writenum,2000);
+
+
+function writenum(){
+  document.getElementById("demo"),innerHTML = document.write(numbers);
+}
+
+
+setTimeout(choosenum,5000);
+
+function choosenum(){
+    const user = parseInt(prompt("inserire i numeri usciti in precedenza"))
+    if (user == numbers) {
+        console.log("hai vinto")
+    } else {
+        console.log("hai perso")
+    }
+}
+
+
